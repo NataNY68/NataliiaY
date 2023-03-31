2d55a454900ef4777a086acf0b06823a1cc82588
nataliayatsishin <nata.yatsishin@gmail.com>
2023-03-31 15:42:57 -0500

    My changes

---
+++ src/test/java/Test/class1.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("hELLO");
+        System.out.println("I am testing gitgit ");
+    }
+}
